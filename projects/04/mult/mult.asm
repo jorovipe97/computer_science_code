14151f2649895b012839834ac31a745cb31e4948
jorovipe97 <jorovipe@gmail.com>
2017-08-24 00:36:40 -0500

    Multplication assembly implemented

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R0 // 0
+D=M
+@sum // 16
+M=0 // RAM[16] = 0
+@i // 17
+M=0
+
+(BLOCK1)
+@R1 // 1
+D=M
+@i // 17
+D=M-D // D=sum-(R1-1), this will be util on conditional
+@BLOCK2
+D;JGE  // Condicion para salirse del while
+// start while
+
+
+@R0
+D=M
+@sum
+M=M+D
+
+
+@i // i++ start
+D=M
+M=D+1 // i++
+@BLOCK1
+0;JMP
+(BLOCK2)
+@sum
+D=M
+@R2
+M=D
+(END)
+@END
+0;JMP
